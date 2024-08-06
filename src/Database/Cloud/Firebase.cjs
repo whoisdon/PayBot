e2e8192541b08505027cbee8ba137a06b27b8cbb
whoisdon <conta.discordbaby@gmail.com>
2024-08-06 00:21:35 -0300

    feat: default config firebase app

---
+++ src/Database/Cloud/Firebase.cjs
@@ -0,0 +1,14 @@
+const { initializeApp } = require('firebase/app');
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+module.exports = initializeApp(firebaseConfig);
