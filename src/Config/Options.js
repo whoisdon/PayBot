684c1782a6def20d56b5dcd93c968cb4c5bd6e2c
whoisdon <conta.discordbaby@gmail.com>
2024-08-06 00:20:35 -0300

    feat: full use

---
+++ src/Config/Options.js
@@ -0,0 +1,28 @@
+import { GatewayIntentBits, Partials } from 'discord.js';
+
+const options = {
+  intents: [
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+    Partials.ThreadMember,
+    Partials.User
+  ]
+};
+
+export default options;
